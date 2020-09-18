84400a721dfa9304f689696d53e36aab4cd2f9b8
ViniciusScatenaRocha <48922991+ViniciusScatenaRocha@users.noreply.github.com>
2020-09-17 22:26:46 -0300

    Add files via upload

---
+++ AC1-pokemon/src/ac1/pokemon/AC1Pokemon.java
@@ -0,0 +1,22 @@
+
+package ac1.pokemon;
+
+public class AC1Pokemon {
+
+    public static void main(String[] args) {
+                //Instanciar um novo objeto
+        Pokemon meuPokemon = new Pokemon("Bulbassaur", "terra", 20);
+        Pokemon outroPokemon = new Pokemon("Charmander", "Agua", 20);
+        Batalha batalha1 = new Batalha();
+
+        meuPokemon.calculaAtributos();
+        outroPokemon.calculaAtributos();
+
+        batalha1.meuPokemon(meuPokemon.nome, meuPokemon.getHP());
+        batalha1.outroPokemon(outroPokemon.nome, outroPokemon.getHP());
+
+        batalha1.start();
+
+    }
+
+}
